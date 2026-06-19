ae57a06dff96489fb0db46503526fc4b78ae7795
shahidansari311 <shahidansari945256@gmail.com>
2026-06-19 23:00:59 +0530

    types inference and annotation

---
+++ first/src/typesInTS.ts
@@ -0,0 +1,22 @@
+let drink="chai";
+
+let cups=Math.random()>0.5 ? 10 : 5 ;
+
+/**
+ * Type inference
+ */
+let channelName="Chai aur Code";
+
+// channelName=12221;
+
+
+/**
+ * Annotation
+*/
+let chaiFlavour:string="Masala Chai";
+
+chaiFlavour="Ginger Tea";
+// chaiFlavour=3; 
+
+// let chaiNumber:number;
+let chaiNumber:boolean;
